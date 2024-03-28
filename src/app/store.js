84944489b0ab23ca0eb3f8544f574f5889c7d2e0
danie odukwe <kingdanny295@gmail.com>
2024-03-28 09:55:53 +0100

    @add/ improve redux by adding the crypto api to the store to make it accessible all over the website

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { cryptoApi } from "../services/CryptoApi";
 
 export const store = configureStore({
     reducer: {
       // Add the generated reducer as a specific top-level slice
-      [pokemonApi.reducerPath]: pokemonApi.reducer,
+      [cryptoApi.reducerPath]: cryptoApi.reducer,
     },
     // Adding the api middleware enables caching, invalidation, polling,
     // and other useful features of `rtk-query`.
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(pokemonApi.middleware),
+      getDefaultMiddleware().concat(cryptoApi.middleware),
   })
