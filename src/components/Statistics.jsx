42d56fb88cc90ab10b460cb6a6daf7c195e2466d
danie odukwe <kingdanny295@gmail.com>
2024-03-29 20:02:30 +0100

    @add/ created a new component to store list of all crypto data so it can be used in any other page

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,17 +1,23 @@
+import millify from 'millify'
 import coin from '../assets/coin.gif'
 import { useGetStatsQuery } from '../services/CryptoApi'
 import Loader from './Loader'
+import { Link } from 'react-router-dom'
+import Cryptos from '../pages/Cryptos'
 
 const Statistics = () => {
 
   const { data, error, isFetching } = useGetStatsQuery()
+
+  // handles statistics api
   if (error) return <p>Error :(</p>
   if (isFetching) return <Loader />
   const stats = data?.data
-  console.log(data)
+  // handles cryptos api
+
   
   return (
-    <section>
+    <section className='flex flex-col gap-8'>
         <div className="md:text-[2.5rem] text-[1.5rem] font-semibold capitalize w-full text-gray-600 flex items-center gap-1">
             <h1>Coin Ranking Global Statistics</h1>
             <img src={coin} alt="coin" width={40}/>
@@ -19,8 +25,98 @@ const Statistics = () => {
         <p className='font-medium'> Gain insight into the Intricacies of Cryptocurrency Trends, Market Dynamics, and Asset Performance.<br/>
             These global statistics tell about the data available on coin ranking
         </p>
-        <div>
+        {/* global*/}
+        <div className='flex flex-col gap-3'>
+          <h1 className='md:text-[2rem] text-[1.5rem] font-semibold capitalize'>Global Crypto stats</h1>
+          <div className='w-full flex justify-between flex-wrap gap-4'>
+            <div>
+              <h3 className='text-gray-600 text-[0.9rem] font-medium capitalize'>Total cryptocurrencies</h3>
+              <h1 className='text-[2rem]'>{millify(stats?.totalCoins)}</h1>
+            </div>
+            <div>
+              <h3 className='text-gray-600 text-[0.9rem] font-medium capitalize'>Total exchanges</h3>
+              <h1 className='text-[2rem]'>{stats?.totalExchanges}</h1>
+            </div>
+            <div>
+              <h3 className='text-gray-600 text-[0.9rem] font-medium capitalize'>total 24h vol</h3>
+              <h1 className='text-[2rem]'>{millify(stats?.total24hVolume)}</h1>
+            </div>
+            <div>
+              <h3 className='text-gray-600 text-[0.9rem] font-medium capitalize'>total market cap</h3>
+              <h1 className='text-[2rem]'>{millify(stats?.totalMarketCap)}</h1>
+            </div>
+            <div>
+              <h3 className='text-gray-600 text-[0.9rem] font-medium capitalize'>total market</h3>
+              <h1 className='text-[2rem]'>{millify(stats?.totalMarkets)}</h1>
+            </div>
+          </div>
+        </div>
 
+        {/* best coin and newest coin */}
+        <div className='flex w-full flex-wrap gap-6 max-w-[1200px] mx-auto'>
+          <div className='w-full flex flex-col gap-6'>
+            <h1 className='md:text-[2rem] text-[1.5rem] font-semibold capitalize'>
+              Best coins to look out for
+            </h1>
+            <div className='w-full flex gap-5 flex-wrap'>
+              {data?.data?.bestCoins.map((coin, i ) => (
+                <Link 
+                  key={i} 
+                  to={coin.coinrankingUrl}
+                  target='_blank'
+                >
+                  <div className='flex flex-col items-center gap-6 border w-[150px] min-h-[100px] p-5 cursor-pointer rounded-xl hover:shadow-md transition-all'>
+                    <img className='w-[30px] h-[30px]' src={coin.iconUrl} alt="icons" />
+                    <div className='flex flex-col items-center gap-3'>
+                      <h1 className='text-2xl'>{coin.name}</h1>
+                      <div>
+                        <p className='text-[0.75rem]'>SYMBOL</p>
+                        <h1 className='text-[0.9rem] font-bold text-gray-600'>{coin.symbol}</h1>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div className='w-full flex flex-col gap-6'>
+            <h1 className='md:text-[2rem] text-[1.5rem] font-semibold capitalize'>
+              Newest coins in the market
+            </h1>
+            <div className='w-full flex gap-5 flex-wrap '>
+              {data?.data?.newestCoins.map((coin, i ) => (
+                <Link 
+                  key={i} 
+                  to={coin.coinrankingUrl}
+                  target='_blank'
+                >
+                  <div className='flex flex-col items-center gap-6 border w-[150px] min-h-[100px] p-5 cursor-pointer rounded-xl hover:shadow-md transition-all'>
+                    <img className='w-[30px] h-[30px]' src={coin.iconUrl} alt="icons" />
+                    <div className='flex flex-col items-center gap-3'>
+                      <h1 className='text-2xl'>{coin.name}</h1>
+                      <div>
+                        <p className='text-[0.75rem]'>SYMBOL</p>
+                        <h1 className='text-[0.9rem] font-bold text-gray-600'>{coin.symbol}</h1>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+        </div>
+        </div>
+        
+        {/* top 10 cryptos */}
+        <div className='w-full flex flex-col gap-6'>
+          <div className='w-full flex items-end justify-between'>
+            <h1>Top 10 Crypto currencies in the world</h1>
+            <Link to='/cryptos'>
+                <p className='text-gray-600 font-medium cursor-pointer hover:text-yellow-400'>see more!</p>
+            </Link>
+          </div>
+
+          <Cryptos simplified/>
         </div>
     </section>
   )
--- src/pages/Cryptos.jsx
+++ src/pages/Cryptos.jsx
@@ -1,8 +1,58 @@
+import { Link } from "react-router-dom"
+import { useGetCryptosQuery } from "../services/CryptoApi"
+import { useState, useEffect } from "react"
 
+const Cryptos = ({simplified}) => {
+  const count = simplified ? 10 : 100
+  const {data, error, isFetching } = useGetCryptosQuery(count) 
+  const [ cryptos, setCryptos ] = useState([])
+  const [ search, setSeach ] = useState("")
 
-const Cryptos = () => {
+  useEffect(() => {
+    const filteredData = data?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()))
+    setCryptos(filteredData)
+    console.log(cryptos)
+  },[search, data])
+
   return (
-    <div>Cryptos</div>
+    <section className="my-5 px-10">
+      <div>
+        {
+          !simplified && (
+            <div className="max-w-[300px] mx-auto px-5 py-3 text-[0.8rem] outline-none border-yellow-400 border">
+              <input
+                type="text"
+                placeholder="search for a currency"
+                onChange={(e) => setSeach(e.target.value)}
+              />
+            </div>
+          )
+        }
+      </div>
+
+      <div className="my-5 flex flex-wrap justify-evenly gap-6">
+        {cryptos.map((data) => (
+          <div key={data.rank} className="w-[200px]">
+            <Link to={`/cryptos/${data.uuid}`}>
+              <div className="border rounded-xl flex flex-col gap-6 hover:shadow-md">
+                <div className="flex w-full items-center justify-between p-5 border-b">
+                  <div className="flex items-center gap-2">
+                    <p>{data.rank}.</p>
+                    <p>{data.name}</p>
+                  </div>
+                  <div>
+                    <img className="w-[30px]" src={data.iconUrl} alt="icon" />
+                  </div>
+                </div>
+                <div>
+
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
